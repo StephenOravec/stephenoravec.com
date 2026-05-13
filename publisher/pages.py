29e5a2c66d613746b43ac6e52d85fe60373faaed
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2026-05-13 17:37:32 -0400

    Add pages.py with static page metadata

---
+++ publisher/pages.py
@@ -0,0 +1,6 @@
+PAGES = [
+    {"url": "/", "lastmod": "2026-02-05", "priority": "1.0"},
+    {"url": "/cablepunk", "lastmod": "2026-05-02", "priority": "0.8"},
+    {"url": "/books", "lastmod": "2026-02-05", "priority": "0.8"},
+    {"url": "/profiles", "lastmod": "2026-02-05", "priority": "0.8"},
+]
